07c9e118596b94c4e735e01e2cc86de0690bb046
atpatat <77606447+atpatat@users.noreply.github.com>
2021-04-16 09:56:24 +0200

    Update priceReaderCompute.py

--- priceReaderCompute.py
+++ priceReaderCompute.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 
-sapVMs=['a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'd11', 'd12', 'd13', 'd14', 'ds11', 'ds12', 'ds13', 'ds14', 'ds11v2', 'ds12v2', 'ds13v2', 'ds14v2', 'ds15v2', 'gs1', 'gs2', 'gs3', 'gs4', 'gs5', 'm64ms', 'm128ms', 'm64s', 'm128s']
+sapVMs=['a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'd11', 'd12', 'd13', 'd14', 'ds11', 'ds12', 'ds13', 'ds14', 'ds11v2', 'ds12v2', 'ds13v2', 'ds14v2', 'ds15v2', 'd2sv3', 'd4sv3', 'd8sv3', 'd16sv3', 'd32sv3', 'd48sv3', 'd64sv3', 'e2sv3', 'e4sv3', 'e8-4s-v3', 'e8sv3', 'e16sv3', 'e20sv3', 'e32sv3', 'e48sv3', 'e64isv3', 'e64sv3', 'd2dsv4', 'd8dsv4', 'd16dsv4', 'd32dsv4', 'd48dsv4', 'd64dsv4', 'e2dsv4', 'e4dsv4', 'e8dsv4', 'e16dsv4', 'e20dsv4', 'e32dsv4', 'e48dsv4', 'e64dsv4', 'gs1', 'gs2', 'gs3', 'gs4', 'gs5', 'm8ms', 'm16ms', 'm32ts', 'm32ls', 'm32ms', 'm64ls', 'm64s', 'm64ms', 'm128s', 'm128ms', 'm208sv2', 'm208msv2', 'm416sv2', 'm416msv2']
 
 payg_vm_price_public_api_url='https://azure.microsoft.com/api/v2/pricing/virtual-machines-base/calculator/?culture=en-us&discount=mosp&currency=eur'
 res_inst_1year_vm_price_public_api_url='https://azure.microsoft.com/api/v2/pricing/virtual-machines-base-one-year/calculator/?culture=en-us&discount=mosp&currency=eur'
